f9d28a5401e013c278ceb579faacc527db0a8be9
Thibault <thibault.courrie@gmail.com>
2017-07-21 16:24:29 +0200

    ca marche mais bof

--- boardcase.rb
+++ boardcase.rb
@@ -1,20 +1,30 @@
+
+
 class BoardCase 
+	
+	initial_board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+	@@initial_board = initial_board
 
 	def initialize(x,y,player)
 		@x = x.to_i
 		@y = y.to_i
 		@player = player
-		#initial_board = [[""]]
-		#@initial_board = initial_board
+		
 	end
 
 
 	def boardcase_update
 
 		if @player == 'joueur_1'
-			@initial_board[@x][@y] = "X"
+			@@initial_board[@x][@y] = "X"
+			p @@initial_board[0]
+			p @@initial_board[1]
+			p @@initial_board[2]
 		else
-			@initial_board[@x][@y] = "O"
+			@@initial_board[@x][@y] = "O"
+			p @@initial_board[0]
+			p @@initial_board[1]
+			p @@initial_board[2]
 		end
 
 	end
